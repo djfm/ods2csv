dc549a6c3af9b88ea9eb14deb4eddedba0629a24
fram <fm.de.jouvencel@gmail.com>
2012-09-06 17:54:51 +0200

    fixed bug with useless empty rows / cells repeated

--- ods2csv.rb
+++ ods2csv.rb
@@ -6,6 +6,8 @@ require 'csv'
 
 COL_SEP = ';'
 
+num_rows = 0
+
 if infile = ARGV[0]
 	outfile = File.basename(infile,'.ods') + '.csv'
 	entries = Archive.read_open_filename infile do |ar|
@@ -13,16 +15,19 @@ if infile = ARGV[0]
 			if entry.pathname == 'content.xml'
 				xml  = XML::Document.string ar.read_data
 				CSV.open(outfile, "w", :col_sep => COL_SEP) do |csv|
-					xml.find('/office:document-content/office:body/office:spreadsheet/table:table/table:table-row').each do |row|
-						[row['number-rows-repeated'].to_i,1].max.times do
+					rows = xml.find('/office:document-content/office:body/office:spreadsheet/table:table/table:table-row')
+					rows.each_with_index do |row, i|
+						if i + 1 < rows.length then repeat_row = [row['number-rows-repeated'].to_i,1].max else repeat_row = 1 end
+						repeat_row.times do
 							csvrow = []
-							row.each  do |cell|
-								[cell['number-columns-repeated'].to_i,1].max.times do
+							row.each_with_index do |cell, j|
+								if j + 1 < row.count then repeat_column = [cell['number-columns-repeated'].to_i,1].max else repeat_column = 1 end
+								repeat_column.times do
 									csvrow << cell.content
 								end
-								
 							end
 							csv << csvrow
+							num_rows += 1
 						end
 					end
 				end
